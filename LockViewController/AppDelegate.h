9c8195dcb3555d6eddc6710f1920fe2937c8f77b
Charlie Wu <charlie.y.wu@gmail.com>
2013-06-07 13:17:08 +1000

    Initial Commit

---
+++ LockViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LockViewController
+//
+//  Created by Charlie Wu on 7/06/13.
+//  Copyright (c) 2013 Charlie Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LockViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LockViewController
+//
+//  Created by Charlie Wu on 7/06/13.
+//  Copyright (c) 2013 Charlie Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
